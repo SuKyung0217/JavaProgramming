634d5dd9e9f8f00d5521df43240dba8443ae9787
SuKyung0217 <91124029+SuKyung0217@users.noreply.github.com>
2021-09-22 22:58:52 +0900

    week2_LAB_practice
    2주차 LAB코드를 연습하고 실행하였습니다.

---
+++ CircleArea.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		Scanner scanner =  new Scanner(System.in);
+		double radius,area;
+		
+		System.out.print("원의 반지름 입력 : ");
+		radius = scanner.nextDouble();
+		
+		area = Math.PI * radius * radius;
+		
+//		System.out.println(area);
+		System.out.printf("%.1f\n", area);
+		scanner.close();
+	}
+
+}
