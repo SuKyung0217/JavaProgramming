e504ca2de59b2641b9ce875a0039a60b1961e0f0
SuKyung0217 <91124029+SuKyung0217@users.noreply.github.com>
2021-09-29 23:09:00 +0900

    210929_Class_Practice

---
+++ BitOperator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BitOperator {
+
+	public static void main(String[] args) {
+		//비트 연산 : 메모리(주소, 콘텐츠)의 컨텐츠 영역의 비트끼리 연산
+		//shift(<<,>>),& ^ |
+		int i, j;
+		Scanner scn = new Scanner(System.in);
+		System.out.print("첫 번째 정수 입력 : ");
+		i = scn.nextInt();
+		System.out.print("두 번째 정수 입력 : ");
+		j = scn.nextInt();
+		
+		System.out.println("~" + i + "=>" + (-i));
+		
+		
+		System.out.println(i + "<<" + j + "==" + (i << j));
+		System.out.println(i + ">>" + j + "==" + (i >> j));
+		System.out.println(i + ">>>" + j + "==" + (i >>> j));
+		System.out.println(i + "&" + j + "==" + (i & j));
+		
+		System.out.println(i + "^" + j + "==" + (i ^ j));
+		System.out.println(i + "| " + j + "==" + (i | j));
+
+	}
+	
+}
